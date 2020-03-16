d4d3f4cdf327e94f9d75d5065355c398fef5f3bf
Leo <losoviz@gmail.com>
2020-03-17 04:50:44 +0800

    Renamed `timelyBoot` to `boot`

--- src/Component.php
+++ src/Component.php
@@ -32,9 +32,9 @@ class Component extends AbstractComponent
      *
      * @return void
      */
-    public static function prematureBoot()
+    public static function beforeBoot()
     {
-        parent::prematureBoot();
+        parent::beforeBoot();
 
         // Initialize classes
         ContainerBuilderUtils::instantiateNamespaceServices(__NAMESPACE__.'\\Hooks');
