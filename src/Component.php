c3fca7996b13eba8baae9c21572ac06a8388397a
Leo <losoviz@gmail.com>
2020-04-13 19:12:20 +0800

    Added declare(strict_types=1);

--- src/Component.php
+++ src/Component.php
@@ -1,4 +1,7 @@
 <?php
+
+declare(strict_types=1);
+
 namespace PoP\EngineWP;
 
 use PoP\Root\Component\AbstractComponent;
--- src/Hooks/TemplateHookSet.php
+++ src/Hooks/TemplateHookSet.php
@@ -1,4 +1,7 @@
 <?php
+
+declare(strict_types=1);
+
 namespace PoP\EngineWP\Hooks;
 
 use PoP\Engine\Hooks\AbstractHookSet;
