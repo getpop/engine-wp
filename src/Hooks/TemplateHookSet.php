444bd7232fe4ca3b1f8ccb98d713d74c8ef3efe2
Tan Choon Tick <cttan@Tans-MacBook-Air.local>
2020-05-06 19:39:29 +0800

    Moved function to helper

--- src/Hooks/TemplateHookSet.php
+++ src/Hooks/TemplateHookSet.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace PoP\EngineWP\Hooks;
 
 use PoP\Engine\Hooks\AbstractHookSet;
-use PoP\EngineWP\Component;
+use PoP\EngineWP\Templates\TemplateHelpers;
 
 class TemplateHookSet extends AbstractHookSet
 {
@@ -20,7 +20,7 @@ class TemplateHookSet extends AbstractHookSet
     {
         // If doing JSON, for sure return json.php which only prints the encoded JSON
         if (doingJson()) {
-            return Component::getTemplatesDir() . '/Output.php';
+            return TemplateHelpers::getTemplateFile();
         }
         return $template;
     }
