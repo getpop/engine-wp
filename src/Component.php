7947c602a6bc00e87ad4d0cce7728efb3fd5b5ba
Leonardo Losoviz <leo@getpop.org>
2020-05-21 19:48:29 +0800

    Added type "void" to functions

--- src/Component.php
+++ src/Component.php
@@ -41,7 +41,7 @@ class Component extends AbstractComponent
     /**
      * Initialize services
      */
-    protected static function doInitialize()
+    protected static function doInitialize(): void
     {
         parent::doInitialize();
         self::initYAMLServices(dirname(__DIR__));
@@ -52,7 +52,7 @@ class Component extends AbstractComponent
      *
      * @return void
      */
-    public static function beforeBoot()
+    public static function beforeBoot(): void
     {
         parent::beforeBoot();
 
