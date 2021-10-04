2cd0e48fca1b682aeeb7f5f42e70dba409c56aec
leoloso <leo@getpop.org>
2021-10-04 09:13:03 +0000

    Merge pull request #1116 from leoloso/make-autowire-functions-final
    Made all autowire functions final

--- src/Hooks/TemplateHookSet.php
+++ src/Hooks/TemplateHookSet.php
@@ -4,17 +4,17 @@ declare(strict_types=1);
 
 namespace PoP\EngineWP\Hooks;
 
-use Symfony\Contracts\Service\Attribute\Required;
 use PoP\ComponentModel\HelperServices\ApplicationStateHelperServiceInterface;
 use PoP\EngineWP\Templates\TemplateHelpers;
 use PoP\Hooks\AbstractHookSet;
+use Symfony\Contracts\Service\Attribute\Required;
 
 class TemplateHookSet extends AbstractHookSet
 {
     protected ApplicationStateHelperServiceInterface $applicationStateHelperService;
 
     #[Required]
-    public function autowireTemplateHookSet(
+    final public function autowireTemplateHookSet(
         ApplicationStateHelperServiceInterface $applicationStateHelperService,
     ): void {
         $this->applicationStateHelperService = $applicationStateHelperService;
